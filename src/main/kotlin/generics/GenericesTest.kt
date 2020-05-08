be2ba64c6ab0cbdda445625162a927b3654d554a
XZQ <xiazhiqiang1987@gmail.com>
2020-05-09 01:44:27 +0800

    提交

--- src/main/kotlin/generics/GenericesTest.kt
+++ src/main/kotlin/generics/GenericesTest.kt
@@ -3,7 +3,7 @@ package generics
 /**
  * @author  XZQ
  * @date  2020/5/9 1:21
- *
+ * https://blog.csdn.net/qq_39969226/article/details/94782338
  */
 fun main(args: Array<String>) {
 
@@ -14,4 +14,22 @@ fun main(args: Array<String>) {
 * */
 fun <T> isEquals(a: T, b: T): Boolean {
     return a == b
+}
+
+/*
+* 多类型参数
+* */
+fun <T, U> addRectangle(a: T, b: U): U {
+    return b
+}
+
+/*
+*泛型约束主要用于泛型函数和泛型类的声明
+* */
+fun <T : Comparable<T>> isEqual(a: T, b: T): Boolean {
+    return a == b
+}
+
+private fun <T : Number> isEqual(a: T, b: T): Boolean {
+    return (a == b)
 }
