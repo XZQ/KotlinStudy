2560e821ab3b9c7b54de2150383aad5a499fea76
zhiqiang.xia1 <zhiqiang.xia1@nio.com>
2024-03-25 14:02:14 +0800

    feat :  add kotlin
    Signed-off-by: zhiqiang.xia1 <zhiqiang.xia1@nio.com>

---
+++ src/main/kotlin/com/xzq/scaled/NavHost.kt
@@ -0,0 +1,17 @@
+package scaled
+
+sealed class TravelAdvisoriesDestination(val route: String) {
+
+    class AboutDestination
+
+    class CountryListDestination : TravelAdvisoriesDestination("AAAAAA")
+
+    object CountryDetailsDestination
+
+    object TravelAdvisoriesNavHost : TravelAdvisoriesDestination("BBBBBB")
+
+}
+
+fun main() {
+    println("==  ${TravelAdvisoriesDestination.TravelAdvisoriesNavHost.route}")
+}
